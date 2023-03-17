330e7f1547d0588b1406f1dd5a46174e1e81c174
theandrew168 <andrew@shallowbrooksoftware.com>
2023-03-17 00:57:52 -0500

    Organize things, be better about DB conn handling

--- src/snippetbox/core.clj
+++ src/snippetbox/core.clj
@@ -1,5 +1,5 @@
 (ns snippetbox.core
-  (:require [compojure.core :refer [defroutes GET POST]]
+  (:require [compojure.core :refer [routes GET POST]]
             [compojure.route :as route]
             [hiccup.page :as html]
             [java-time.api :as jt]
@@ -8,11 +8,18 @@
             [org.httpkit.server :as httpd])
   (:gen-class))
 
-(def db "jdbc:postgresql://postgres:postgres@localhost:5432/postgres")
 
-(defn migrate [db]
+(def conf {:db "jdbc:postgresql://postgres:postgres@localhost:5432/postgres"})
+
+;; database connection
+
+(defn connect [db]
+  (dt/read-as-instant)
+  (jdbc/get-datasource db))
+
+(defn migrate [conn]
   (jdbc/execute!
-   db
+   conn
    ["CREATE TABLE IF NOT EXISTS snippet (
      id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
      title TEXT NOT NULL,
@@ -21,33 +28,37 @@
      expires TIMESTAMPTZ NOT NULL
      )"]))
 
-(defn snippet-create [db title content expires]
-  (let [created (jt/local-date-time)
-        expires (jt/plus created (jt/days expires))]
+;; data access layer
+
+(defn snippet-create [conn title content expires]
+  (let [now (jt/instant)
+        expires (jt/plus now (jt/days expires))]
     (jdbc/execute-one!
-     db
+     conn
      ["INSERT INTO snippet (title, content, created, expires)
-       VALUES (?, ?, ?, ?) RETURNING id" title content created expires])))
+       VALUES (?, ?, ?, ?) RETURNING id" title content now expires])))
 
-(defn snippet-list [db]
+(defn snippet-list [conn at n]
   (jdbc/execute!
-   db
-   ["SELECT * FROM snippet"]))
+   conn
+   ["SELECT * FROM snippet WHERE expires > ? ORDER BY created DESC LIMIT ?" at n]))
 
-(defn snippet-read [db id]
+(defn snippet-read [conn id at]
   (jdbc/execute-one!
-   db
-   ["SELECT * FROM snippet WHERE id = ?" id]))
+   conn
+   ["SELECT * FROM snippet WHERE id = ? AND expires > ?" id at]))
 
-(defn snippet-update [db id title content]
+(defn snippet-update [conn id title content]
   (jdbc/execute-one!
-   db
+   conn
    ["UPDATE snippet SET title = ?, content = ? WHERE id = ?" title content id]))
 
-(defn snippet-delete [db id]
+(defn snippet-delete [conn id]
   (jdbc/execute-one!
-   db
+   conn
    ["DELETE FROM snippet WHERE id = ?" id]))
+
+;; HTML rendering
 
 (defn render-page [title main]
   (html/html5
@@ -76,43 +87,76 @@
     [:h2 "Latest Snippets"]
     [:p "There's nothing to see here yet!"]]))
 
+;; base response helpers
+
 (defn html-response [code body]
   {:status code
    :headers {"Content-Type" "text/html; charset=utf8"}
    :body body})
 
+(defn redirect-response [code body url]
+  {:status code
+   :headers {"Location" url}
+   :body body})
+
+;; response helpers
+
 (defn ok [body]
   (html-response 200 body))
 
-(defn not-found [_]
-  (html-response 404  "Not found"))
+(defn created []
+  (html-response 201 "Created"))
 
-(defn index [_]
+(defn found [url]
+  (redirect-response 302 "Found" url))
+
+(defn see-other [url]
+  (redirect-response 303 "See Other" url))
+
+(defn not-found [_]
+  (html-response 404 "Not Found"))
+
+;; handlers
+
+(defn index [_ _]
   (ok (render-index)))
 
-(defn view [req]
-  (if-let [id (parse-long (-> req :params :id))]
-    (str "id = " id)
-    (not-found req)))
+(defn view [conn req]
+  (let [now (jt/instant)
+        id (parse-long (-> req :params :id))
+        snippet (snippet-read conn id now)]
+    (if snippet
+      (render-page "View" snippet)
+      (not-found req))))
 
-(defn create [_]
-  (ok "Create a new snippet form..."))
+(defn create [_ _]
+  (ok "Create snippet..."))
 
-(defn submit [_]
-  (ok "Submit a new snippet..."))
+(defn submit [conn _]
+  (let [res (snippet-create conn "O Snail" "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa" 7)
+        id (:snippet/id res)
+        url (format "/snippet/view/%d" id)]
+    (see-other url)))
 
-(defroutes app
-  (GET "/" [] index)
-  (GET "/snippet/view/:id" [] view)
-  (GET "/snippet/create" [] create)
-  (POST "/snippet/create" [] submit)
-  (route/resources "/")
-  (route/not-found not-found))
+;; routes
+
+(defn init-app [conf]
+  (let [conn (connect (:db conf))]
+    (routes
+     (GET "/" [] (partial index conn))
+     (GET "/snippet/view/:id" [] (partial view conn))
+     (GET "/snippet/create" [] (partial create conn))
+     (POST "/snippet/create" [] (partial submit conn))
+     (route/resources "/")
+     (route/not-found not-found))))
+
+
+;; main
 
 (defn -main [& args]
   (let [n-cpu (.availableProcessors (Runtime/getRuntime))
-        port (-> "PORT" System/getenv (or "5000") Integer/parseInt)]
-    (dt/read-as-instant)
+        port (-> "PORT" System/getenv (or "5000") Integer/parseInt)
+        app (init-app conf)]
     (println (format "Listening on port %s..." port))
     (httpd/run-server app {:ip "127.0.0.1"
                            :port port
@@ -121,24 +165,25 @@
 (comment
 
   ;; start the web server
-  (def server (httpd/run-server #'app {:ip "127.0.0.1" :port 5000}))
+  (def server (httpd/run-server (init-app conf) {:ip "127.0.0.1" :port 5000}))
 
   ;; stop the web server
   (server)
 
-  (dt/read-as-instant)
+  ;; connect to database
+  (def conn (connect (:db conf)))
 
   ;; apply migration(s)
-  (migrate db)
+  (migrate conn)
 
   ;; undo migration(s)
-  (jdbc/execute! db ["DROP TABLE IF EXISTS snippet"])
+  (jdbc/execute! conn ["DROP TABLE IF EXISTS snippet"])
 
-  (snippet-create db "Foo" "A tale about foo" 7)
-  (snippet-list db)
-  (snippet-read db 1)
-  (snippet-update db 1 "Bar", "update the content")
-  (snippet-delete db 1)
+  (snippet-create conn "Foo" "A tale about foo" 7)
+  (snippet-list conn)
+  (snippet-read conn 1 (jt/instant))
+  (snippet-update conn 1 "Bar", "update the content")
+  (snippet-delete conn 1)
 
   (render-page "Foo" "asdf")
 
